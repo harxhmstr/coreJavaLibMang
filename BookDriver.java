1679178db51be20c0998b2dfbf25af88f8e49060
harxhmstr <harsh.cllg@gmail.com>
2026-06-19 21:05:13 +0530

    done

--- BookDriver.java
+++ BookDriver.java
@@ -1,16 +1,35 @@
 package LibraryManagment;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Scanner;
 public class BookDriver {
 	static ArrayList<LibraryItem> list = new ArrayList<>();
+	static HashMap<Long,LibraryItem> listMap = new HashMap<>();
 	static Scanner sc = new Scanner(System.in);
 	public static void main(String[] args) {
-		list.add(new Book("The Mistak","Elle Kennedy",78547855,true));
-		list.add(new Book("The Score","Elle Kennedy",58965441,false));
-		list.add(new Book("Mathematics","RD sharma",896441,true));
-		list.add(new Book("calss 10 science","Albert croxs",985222,false));
-		list.add(new Book("The Deal","Elle Kennedy",78547856,true));
-		list.add(new Book("Rich Dad Poor Dad","Alex berk",156324,true));
+		Book b1 = new Book("The Mistak","Elle Kennedy",78547855,true);
+		list.add(b1);
+		listMap.put(b1.isbn, b1);
+		
+		Book b2 = new Book("The Score","Elle Kennedy",58965441,false);
+		list.add(b2);
+		listMap.put(b2.isbn, b2);
+		
+		Book b3 = new Book("Mathematics","RD sharma",896441,true);
+		list.add(b3);
+		listMap.put(b3.isbn, b3);
+		
+		Book b4 = new Book("calss 10 science","Albert croxs",985222,false);
+		list.add(b4);
+		listMap.put(b4.isbn, b4);
+		
+		Book b5 = new Book("The Deal","Elle Kennedy",78547856,true);
+		list.add(b5);
+		listMap.put(b5.isbn, b5);
+		
+		Book b6 = new Book("Rich Dad Poor Dad","Alex berk",156324,true);
+		list.add(b6);
+		listMap.put(b6.isbn, b6);
 		
 		System.out.println("=====Weclcome to programe========");
 		showMenu();
@@ -32,17 +51,17 @@ public class BookDriver {
 		case 2:
 			for(LibraryItem x:list) {
 				x.getDetails();
-				System.out.println("===========");
+				System.out.println("========================================");
 			}
 			break;
 		case 3:
-			//search();
+			search();
 			break;
 		case 4:
-			//borrow();
+			borrow();
 			break;
 		case 5: 
-			//return_();
+			return_();
 			break;
 		case 6:
 			System.exit(0);
@@ -62,5 +81,39 @@ public class BookDriver {
         long isbn = sc.nextLong();
         Book b1=new Book(title,author,isbn,true);
         list.add(b1);
+        listMap.put(isbn, b1);
 	}
+	
+	static Book search() {
+		System.out.println("Enter the isbn number");
+		LibraryItem found = listMap.get(sc.nextLong());
+		if(found == null) {
+			System.out.println("Book not found");
+			return null;
+		}
+		found.getDetails();
+		return (Book)found;
+	}
+	
+	static void borrow() {
+		Book b= search();
+		if(b.available!=true) {
+			System.out.println("book cannot be borrowed");
+			return;
+		}
+		
+		System.out.println("do you want to borrow/n1) YES/n2) NO");
+		int choice =  sc.nextInt();
+		if(choice==1) {
+			System.out.println("borrow approved ");
+			b.available=false;
+		}		
+	}
+	
+	static void return_() {
+		Book b = (Book)search();
+		System.out.println("returned sucessfully");
+		b.available=true;
+	}
+	
 }
